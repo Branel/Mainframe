a4907969c4b06bc32ec6f1b725efc4849cc3879f
Branel <emirpod02@gmail.com>
2023-04-21 20:52:48 +0200

    update to 1.19.4

---
+++ src/com/branel/dashboard/events/eliminationstreak.java
@@ -0,0 +1,4 @@
+
+// The idea of this event is to reward a streak of eliminating other players, this streak would be ended by player death, first draft would be applying stronger enchants to the equipment worn by the user as reward
+// In further implementations, it would be ideal to reward different rewards depending on the enviroment of the map, in arenas where users are more succeptible to fall damage they would also be rewarded feather falling (up to level 4) for every elimination.
+
